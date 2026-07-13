38c84a7768f8ca9f184f2327acbda4f262fd94e1
anshdholakiya <dholakiyaansh1707@gmail.com>
2026-07-13 14:08:55 +0530

    another mistake restored 😂

--- Mongoose/src/app.js
+++ Mongoose/src/app.js
@@ -1,11 +1,10 @@
+require("dotenv").config(); // load env file
 const express = require("express");
 const noteModel = require("./models/note.model.js");
-
 
 // server created
 const app = express();
 app.use(express.json());
-
 
 /* 
 
@@ -18,23 +17,20 @@ DELETE /notes/:id => delete
 */
 
 app.post("/notes", async (req, res) => {
-    const data = req.body  // get data from body 
+    const data = req.body; // get data from body
 
     await noteModel.create({
         title: data.title,
-        description: data.description
-    }) // karan ke data mubmai na server ma jay store thay and response mokle to thodi var lage  ne bhai
+        description: data.description,
+    }); // karan ke data mubmai na server ma jay store thay and response mokle to thodi var lage  ne bhai
 
-    res.status(201).json({ message: "Notes Created Succesfully" })
+    res.status(201).json({ message: "Notes Created Succesfully" });
 
-    console.log("somehting added")
-})
-
-
+    console.log("somehting added");
+});
 
 app.get("/notes", async (req, res) => {
-
-    const notes = await noteModel.find();  //! find alwayas return []  and find bdhuy retur kre noteModel mathi
+    const notes = await noteModel.find(); //! find alwayas return []  and find bdhuy retur kre noteModel mathi
 
     // const notes = await noteModel.findOne({   //! findOne always return {} object
     //     title: "Test Title 1v"  //* it can also return null or empty array accoringly
@@ -46,9 +42,9 @@ app.get("/notes", async (req, res) => {
 
     res.status(200).json({
         Message: "Notes fetch successfully",
-        notes: notes
-    })
-})
+        notes: notes,
+    });
+});
 /*
     find => [{},{}] or []
     findOne => {} or null
@@ -58,33 +54,28 @@ app.delete("/notes/:id", async (req, res) => {
     const id = req.params.id;
 
     await noteModel.findOneAndDelete({
-        _id: id
-    })
+        _id: id,
+    });
 
     res.status(200).json({
-        message: "Note deleted successfully"
+        message: "Note deleted successfully",
     });
-})
-
+});
 
 app.patch("/notes/:id", async (req, res) => {
     const id = req.params.id;
     const description = req.body.description;
 
-    await noteModel.findOneAndUpdate({ _id: id }, { description: description })  // findOneAndUpdate awlays take two object
+    await noteModel.findOneAndUpdate({ _id: id }, { description: description }); // findOneAndUpdate awlays take two object
 
     res.status(200).json({
-        message: "Note updated successfully"
+        message: "Note updated successfully",
     });
-})
-
-
-
+});
 
 // app.get("/notes",(req,res) =>{
 //     await noteModel.
 // })
-
 
 /* note = {title,description} */
 
--- Mongoose/src/db/db.js
+++ Mongoose/src/db/db.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 async function connectDB() {  //* connect server to database
 
-    //! "mongodb+srv://ansh:uuyGmfUr9pHlGUQE@complete-backend-ansh.gvqrjam.mongodb.net"  this is cluster url not database okay
-    await mongoose.connect("mongodb://ansh:uuyGmfUr9pHlGUQE@ac-p3rcobr-shard-00-00.gvqrjam.mongodb.net:27017,ac-p3rcobr-shard-00-01.gvqrjam.mongodb.net:27017,ac-p3rcobr-shard-00-02.gvqrjam.mongodb.net:27017/hally?ssl=true&replicaSet=atlas-o76c6p-shard-0&authSource=admin&appName=Complete-Backend-Ansh");
+    //! "mongodb+srv://ansh:<password>@complete-backend-ansh.gvqrjam.mongodb.net"  this is cluster url not database okay
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log("connected to db");
 }
